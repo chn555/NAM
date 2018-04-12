d40f49bd76095763103c01433f45379223c71185
chn555 <chn566@gmail.com>
2018-04-13 02:25:58 +0300

    df

--- functions.sh
+++ functions.sh
@@ -2,3 +2,8 @@
 #
 # written by chn555
 # this script will use nmcli to set up static ip efortlessly.
+
+# the basic command is
+# nmcli con clone "chn555 5GHZ" "chn555 5GHZ static"
+
+# nmcli con mod "chn555 5GHZ static" ipv4.method manual ipv4.addr "192.168.0.155/24" ipv4.gateway "192.168.0.1" ipv4.dns "8.8.8.8"
