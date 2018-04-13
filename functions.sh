d01809eaecc5daa9d73d310cb1ce569f37f3e301
chn555 <chn566@gmail.com>
2018-04-13 17:52:03 +0300

    sd

--- functions.sh
+++ functions.sh
@@ -7,3 +7,43 @@
 # nmcli con clone "chn555 5GHZ" "chn555 5GHZ static"
 
 # nmcli con mod "chn555 5GHZ static" ipv4.method manual ipv4.addr "192.168.0.155/24" ipv4.gateway "192.168.0.1" ipv4.dns "8.8.8.8"
+
+
+Filter_Active_Interfaces () {
+  Active_Interfaces=($(nmcli -t con show --active | cut -d ":" -f 4))
+  echo ${Active_Interfaces[*]}
+}
+
+createmenu (){
+  arrsize=$1
+  echo "Size of array: $arrsize"
+  echo "${@:2}"
+  select option in "${@:2}"; do
+    if [ "$REPLY" -eq "$arrsize" ];
+    then
+      echo "Exiting..."
+      break;
+    elif [ 1 -le "$REPLY" ] && [ "$REPLY" -le $((arrsize-1)) ];
+    then
+      echo "You selected $option which is option $REPLY"
+      break;
+    else
+      echo "Incorrect Input: Select a number 1-$arrsize"
+    fi
+  done
+}
+
+Interface_IP () {
+    declare IP=$(ip addr show $option | awk '{if(NR==3) print $2}' )
+    echo $IP
+}
+
+Clone_Profile () {
+  Active_Profile=$( nmcli -t con show --active |  awk '{if(NR==1) print $0}'|cut -d ":" -f 1 )
+  echo $Active_Profile
+}
+
+Filter_Active_Interfaces
+createmenu "${#Active_Interfaces[@]}" "${Active_Interfaces[@]}"
+Interface_IP
+Clone_Profile
