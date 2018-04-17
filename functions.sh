274b565c506b8808cbdcaeff646fae539172c1b5
chn555 <chn566@gmail.com>
2018-04-18 01:41:15 +0300

    added verification

--- functions.sh
+++ functions.sh
@@ -10,6 +10,9 @@
 
 # Filter out the active interfaces, then keep only the ones that are real, like wireless and ethernet.
 # Filter the remaining ones and extract the actual interface name
+
+line=$(printf '%40s\n' | tr ' ' -)
+echo $line
 Filter_Active_Interfaces () {
   echo Looking for active interfaces...
   echo ""
@@ -71,6 +74,12 @@ Interface_Info () {
 }
 
 User_Prompt () {
+  # The next 2 lines are used later to validate ipv4 addresses
+  oct='([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
+  ip4="^$oct\\.$oct\\.$oct\\.$oct$"
+  # these set the default DNS, using google. yes Alex i love being spied on.
+  DNS1="8.8.8.8"
+  DNS2="8.8.4.4"
   echo "Please enter the information to be set."
   echo "If a field is left blank, the current setting will be used"
   sleep 1
@@ -78,33 +87,99 @@ User_Prompt () {
   read -p "Enter desired IP address [$Ip] : " New_Ip
   if [[ $New_Ip == "" ]]; then
     New_Ip=$Ip
-  elif ! [[ $New_Ip =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$  ]]; then
-    echo "Invalid IP address, please try again"
-    read -p "Enter desired IP address [$Ip] : " New_Ip
   else
-    :
+    until [[  $New_Ip == "" ]] || [[  "$New_Ip" =~ $ip4 ]]; do
+      read -p "Not a valid IP address. Re-enter [$Ip]  : " New_Ip
+    done
+    if [[ $New_Ip == "" ]]; then
+      New_Ip=$Ip
+    fi
   fi
-  echo $New_Ip
+  echo $line
   read -p "Enter desired Netmask [$Netmask] : " New_Netmask
   if [[ $New_Netmask == "" ]]; then
     New_Netmask=$Netmask
   else
-    :
+    while [[ ! $New_Netmask == "" ]] && [[ ! "$New_Netmask" -gt 1  ||  ! $New_Netmask -lt 32 ]]  ; do
+      read -p "Not a valid netmask, please re-enter your netmask in bit-length format [$Netmask] : " New_Netmask
+    done
+    if [[ $New_Netmask == "" ]]; then
+      New_Netmask=$Netmask
+    fi
   fi
-  echo $New_Netmask
+  echo $line
   read -p "Enter desired Gateway [$Gateway] : " New_Gateway
   if [[ $New_Gateway == "" ]]; then
     New_Gateway=$Gateway
   else
-    :
+    while [[ ! $New_Gateway == "" ]] && [[ ! "$New_Gateway" =~ $ip4 ]]; do
+      read -p "Not a valid Gateway. Re-enter [$Gateway] :  " New_Gateway
+    done
+    if [[ $New_Gateway == "" ]]; then
+      New_Gateway=$Gateway
+    fi
   fi
-  echo $New_Gateway
+  echo $line
+  read -p "Enter desired primary DNS [$DNS1] : " New_DNS1
+  if [[ $New_DNS1 == "" ]]; then
+    New_DNS1=$DNS1
+  else
+    while [[ ! $New_DNS1 == "" ]] && [[ ! "$New_DNS1" =~ $ip4 ]]; do
+      read -p "Not a valid DNS. Re-enter [$DNS1] :  " New_DNS1
+    done
+    if [[ $New_DNS1 == "" ]]; then
+      New_DNS1=$DNS1
+    fi
+  fi
+  echo $line
+  read -p "Enter desired secondary DNS [$DNS2] : " New_DNS2
+  if [[ $New_DNS2 == "" ]]; then
+    New_DNS2=$DNS2
+  else
+    while [[ ! $New_DNS2 == "" ]] && [[ ! "$New_DNS2" =~ $ip4 ]]; do
+      read -p "Not a valid DNS. Re-enter [$DNS2] :  " New_DNS1
+    done
+    if [[ $New_DNS2 == "" ]]; then
+      New_DNS1=$DNS2
+    fi
+  fi
+  Verify_Info
 }
 
-Clone_Profile () {
-  Active_Profile=$( nmcli --t -f NAME,UUID,TYPE,DEVICE con show --active |  awk '{if(NR==1) print $0}'|cut -d ":" -f 1 )
-  echo -e $Active_Profile "is the active profile,\nand will be cloned to" $Active_Profile"static"
+Verify_Info () {
+  echo $line
+  echo $line
+  echo "IP address : $New_Ip/$New_Netmask"
+  echo " "
+  echo "Gateway : $New_Gateway"
+  echo " "
+  echo "Primary DNS : $New_DNS1"
+  echo " "
+  echo "Secondary DNS : $New_DNS2"
+  echo " "
+  echo " "
+  Verify_Info_loop
 }
+
+Verify_Info_loop () {
+  read -p "Is the information correct? [Y,n]" currect
+  if [[ $currect == "" ]] || [[ $currect == "y" ]] || [[ $currect == "Y" ]]; then
+    :
+  elif [[ $currect == "n" ]] || [[ $currect == "N" ]]; then
+    echo " "
+    echo $line
+    echo $line
+    User_Prompt
+  else
+    echo "Invalid input, try again"
+    Verify_Info_loop
+  fi
+}
+
+#Clone_Profile () {
+#  Active_Profile=$( nmcli --t -f NAME,UUID,TYPE,DEVICE con show --active |  awk '{if(NR==1) print $0}'|cut -d ":" -f 1 )
+#  echo -e $Active_Profile "is the active profile,\nand will be cloned to" $Active_Profile"static"
+# }
 
 #IP_Addr_Prompt () {}
 
