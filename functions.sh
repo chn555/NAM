bc9935e668038ae48cbff7d8726aa5e8e9b8f2b3
chn555 <chn566@gmail.com>
2018-04-22 20:09:24 +0300

    sd

--- functions.sh
+++ functions.sh
@@ -215,8 +215,9 @@ Clone_Profile () {
   sleep 1s
   echo "Cloning profile..."
   nmcli con clone "$Active_Profile" "$New_Profile"
-  #nmcli con mod "$New_Profile" ipv4.method manual ipv4.addr "$New_Ip" ipv4.gateway "$New_Gateway" ipv4.dns "$New_DNS1 $New_DNS2"
-
+  nmcli con mod "$New_Profile" ipv4.method manual ipv4.addr "$New_Ip" ipv4.gateway "$New_Gateway" ipv4.dns "$New_DNS1 $New_DNS2"
+  nmcli con down $Active_Profile && nmcli con up $New_Profile
+  echo "Profile $New_Profile activated"
  }
 
 
